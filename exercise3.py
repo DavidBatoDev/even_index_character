0038fc850866844f180a91cc99a31ee2b6517a1d
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 14:45:47 +0800

    Done with pseudocode

---
+++ exercise3.py
@@ -0,0 +1,4 @@
+# define a function with a parameter of string
+# inside a function, we loop using range based on the length of the string
+# only print every after 2 index (provide a third arguement to the for loop)
+# Ask user for a word, then run the function
