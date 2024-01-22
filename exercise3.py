455bfb6b1d7e36cc5c4f806c47a3b6db3de40dc6
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 14:48:12 +0800

    Done with the code

--- exercise3.py
+++ exercise3.py
@@ -1,4 +1,16 @@
 # define a function with a parameter of string
+def printing_even_index_on_str(word):
+    print(f"Orginal String is {word}")
+    print("Printing only even index chars")
 # inside a function, we loop using range based on the length of the string
-# only print every after 2 index (provide a third arguement to the for loop)
+    word_length = len(word)
+    for i in range(0, word_length, 2):
+        # only print every after 2 index (provide a third arguement to the for loop)
+        print(word[i])
+
 # Ask user for a word, then run the function
+user_input = input("Whats the word? ")
+printing_even_index_on_str(user_input)
+
+
+
